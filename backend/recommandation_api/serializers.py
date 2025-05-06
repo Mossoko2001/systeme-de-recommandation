66b0669c4b077ff5130b7164623d1ccf8270da08
Mossoko2001 <petitcamara2001@gmail.com>
2025-05-06 11:59:41 +0000

    Docummantation API

--- backend/recommandation_api/serializers.py
+++ backend/recommandation_api/serializers.py
@@ -1,4 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'profile_picture', 'favorite_genres')
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'profile_picture', 'favorite_genres')
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            profile_picture=validated_data.get('profile_picture'),
+            favorite_genres=validated_data.get('favorite_genres')
+        )
+        return user
+
 
 class RecommendMovieSerializer(serializers.Serializer):
     item_id = serializers.IntegerField()
@@ -16,15 +50,15 @@ class RecommendBookSerializer(serializers.Serializer):
     small_image_url = serializers.URLField()
 
 class UserRecommendParamsSerializer(serializers.Serializer):
-    user_id = serializers.IntegerField()
+    # user_id = serializers.IntegerField()
     dataset_type = serializers.ChoiceField(choices=['books', 'movies'])
     top_n = serializers.IntegerField(required=False, default=5)
-    weight_content = serializers.FloatField(required=False, default=0.5)
-    weight_collaborative = serializers.FloatField(required=False, default=0.5)
+    # weight_content = serializers.FloatField(required=False, default=0.5)
+    # weight_collaborative = serializers.FloatField(required=False, default=0.5)
 
 class ItemRecommendParamsSerializer(serializers.Serializer):
     item_id = serializers.IntegerField()
     dataset_type = serializers.ChoiceField(choices=['books', 'movies'])
     top_n = serializers.IntegerField(required=False, default=5)
-    weight_content = serializers.FloatField(required=False, default=0.5)
-    weight_collaborative = serializers.FloatField(required=False, default=0.5)
+    # weight_content = serializers.FloatField(required=False, default=0.5)
+    # weight_collaborative = serializers.FloatField(required=False, default=0.5)
--- backend/recommandation_api/urls.py
+++ backend/recommandation_api/urls.py
@@ -1,7 +1,45 @@
 from django.urls import path
-from .views import UserRecommendView, ItemRecommendView
+from .views import (UserRecommendView, 
+                    ItemRecommendView, 
+                    RegisterView, 
+                    LoginView, 
+                    UserProfileView,
+                    UserRecommendView
+                )
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Recommandation",
+      default_version='v1',
+      description="API pour le système de recommandation de films/livres",
+      contact=openapi.Contact(email="camara13fs@gmail.com"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('recommend/user/', UserRecommendView.as_view(), name='recommend-user'),
     path('recommend/item/', ItemRecommendView.as_view(), name='recommend-item'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+
+    # Docs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]# backend/recommandation_api/urls.py
+
+"""
+Mossoko
+{
+  "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2NTM1NzQwLCJpYXQiOjE3NDY1MzExNzksImp0aSI6ImYwOGEwMjY3MjFkZDQ1ZTU4ODgxMTRiOTNkYjBhMzlmIiwidXNlcl9pZCI6MSwidXNlcm5hbWUiOiJNb3Nzb2tvIn0.nlbIv3uNVjUNoAI8HrGbnFiIy2_aUZpG_iZzrJATHUY",
+  "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc0NzEzNjk0MCwiaWF0IjoxNzQ2NTMyMTQwLCJqdGkiOiIxZjg5NTYyZjVkYmM0YzFjYWNjNTFmODNjMmNjNzJiMiIsInVzZXJfaWQiOjEsInVzZXJuYW1lIjoiTW9zc29rbyJ9.Cgm7HW00MKivb_6SnJp7xxYmixQnrjaYNr3gW8RgK9Q"
+}
+"""
